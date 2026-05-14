67eaf8cfa87fc72efdfb1dae05f538ab09cbbd03
Yahya Gilany <yahya.gilany@live.com>
2026-05-13 23:22:11 -0400

    feat(studio): bootstrap AskDB env in studio bin
    Invoke bootstrapAskDbEnv before the Studio CLI so cwd .env and
    askdb.config are merged like other first-party entrypoints.

--- apps/studio/src/bin.ts
+++ apps/studio/src/bin.ts
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+import { bootstrapAskDbEnv } from "@askdb/config";
 import { runStudioCli } from "./cli.js";
+
+bootstrapAskDbEnv({ cwd: process.cwd() });
 
 process.exit(await runStudioCli(process.argv.slice(2)));
